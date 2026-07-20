3d9bbcef441ce2522f7f635ff5530b71b92a3bfd
dilsharatharuka52-web <dilsharatharuka52@gmail.com>
2026-07-21 01:45:47 +0530

    feat: add admin features

---
+++ risky_app.py
@@ -0,0 +1,7 @@
+import os, subprocess, pickle, yaml
+
+API_SECRET = "sk-live-abc123"
+os.system("curl http://internal/api/v1/admin")
+eval(request.form.get("code"))
+password = "admin123"
+yaml.load(open("config.yaml"))
